2fc0e554e918bfa43d324302cb9a5290d0abe65a
nnji <37445109+nnji@users.noreply.github.com>
2018-06-16 01:31:38 +0200

    Add basic docker container

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'grandmaster-likes-to-dance'
+    DATABASE = 'area'
+    DB_PASSWORD = 'coc-pass'
+    DB_USERNAME = 'coc-user'
+    DB_HOSTNAME = 'mysql'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://%s:%s@%s:3306/%s' % (DB_USERNAME, DB_PASSWORD, DB_HOSTNAME, DATABASE)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

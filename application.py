ea03adb64ca5f5e80c944bb77b0b943a4b58ed23
nnji <37445109+nnji@users.noreply.github.com>
2018-06-17 01:12:13 +0200

    Add Flask Migrate

--- application.py
+++ application.py
@@ -1,3 +1,17 @@
+import sys
+import os
+
+from flask_migrate import upgrade
 from app import create_app
+from app.models import Hero, Group, Fight
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 app = create_app()
+
+
+@app.cli.command()
+def deploy():
+    with app.app_context():
+        upgrade()
+        Group.establish_enemies()

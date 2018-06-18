6a087b6a4de8d83793c9b8071e699358e28ef77e
Marcin <josinski@outlook.com>
2018-06-18 03:05:29 +0200

    Add token_required and permission_required decorators

---
+++ app/api/decorators.py
@@ -0,0 +1,50 @@
+from functools import wraps
+
+import jwt
+from flask import request, current_app, g
+
+from app.models import Hero, Permission
+from . import errors
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return errors.unauthorized('Token is missing')
+
+        try:
+            data = jwt.decode(token, current_app.config['SECRET_KEY'])
+            hero = Hero.query.filter_by(public_id=data['public_id']).first()
+
+            g.current_hero = hero
+
+        except jwt.ExpiredSignatureError:
+            return errors.unauthorized('Signature has expired')
+        except:
+            return errors.unauthorized('Token is invalid') # 401
+
+        return f(hero, *args, **kwargs)
+
+    return decorated
+
+
+def permission_required(permission):
+    def decorator(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            if g.current_hero.permissions != permission:
+                return errors.unauthorized('Permission failed')
+            return f(*args, **kwargs)
+        return decorated
+    return decorator
+
+
+def grandmaster_required(f):
+    return permission_required(Permission.ADMIN)(f)
+
